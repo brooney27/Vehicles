59efd0a435452c7864e535d442c0cc62bc09bd79
brooney27 <brooney278@gmail.com>
2016-06-20 10:05:49 -0400

    ayy

---
+++ src/Vehicle.java
@@ -0,0 +1,16 @@
+
+public abstract class Vehicle {
+	
+	public void accelerate(){
+		System.out.println("The vehicle speeds up");
+	}
+	
+	public void run(){
+		System.out.println("The vehicle begins running");
+	}
+	
+	public void stop(){
+		System.out.println("The vehicle stops");
+	}
+	
+}
