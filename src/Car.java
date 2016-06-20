3466bde9c929beda4e0ff176ab7beb7f75deb984
brooney27 <brooney278@gmail.com>
2016-06-20 10:22:20 -0400

    ayy

---
+++ src/Car.java
@@ -0,0 +1,54 @@
+
+public class Car extends Vehicle{
+	private String color;
+	private String model;
+	private int speed;
+	
+	public Car(){
+		color = "Blue";
+		model = "Camry";
+		speed = 0;
+	}
+	
+	public Car(String color, String model){
+		this.color = color;
+		this.model = model;
+		speed = 0;
+	}
+	
+	public void setColor(String input){
+		color = input;
+	}
+	public String getColor(){
+		return color;
+	}
+	
+	public void setModel(String input){
+		model = input;
+	}
+	public String getModel(){
+		return model;
+	}
+	
+	public void setSpeed(int input){
+		speed = input;
+	}
+	public int getSpeed(){
+		return speed;
+	}
+	
+	public void run(){
+		System.out.println("The " + color + " " + model + " starts");
+	}
+	
+	public void accelerate(){
+		speed+=10;
+		System.out.println("The " + color + model + " speeds up to " + speed + " mph");
+	}
+	
+	public void stop(){
+		speed = 0;
+		System.out.println("The " + color + " " + model + " stops");
+	}
+	
+}
--- src/VehicleApp.java
+++ src/VehicleApp.java
@@ -3,7 +3,15 @@ public class VehicleApp {
 	
 	public static void main(String[] args){
 		
+		Car c = new Car();
+		Car d = new Car("Green", "Dodge");
 		
-		
+		c.run();
+		d.run();
+		c.accelerate();
+		d.accelerate();
+		d.accelerate();
+		c.stop();
+		d.stop();
 	}
 }
